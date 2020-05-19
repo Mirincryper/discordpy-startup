bdcaf436278aed581b621a28ea8c43594550957f
みりんくりーぱー <62057540+Mirincryper@users.noreply.github.com>
2020-05-19 22:19:28 +0900

    機能追加
    コマンドプレフィックスを/からsk\に変更。動かなければ別のものに。
    sk\mirinの追加
    アクティビティの指定、駄目だったら後々直す。
    sk\helpの追加。ここでの改行が文章にも反映されるのかどうか、反映されなければ解決方法聞くしかない

--- discordbot.py
+++ discordbot.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
+bot = commands.Bot(command_prefix='sk\')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
@@ -12,10 +12,25 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
+await client.change_presence(activity=discord.Game(name='my game'))
+
+# or, for watching:
+activity = discord.Activity(name='test', type=discord.ActivityType.watching)
+await client.change_presence(activity=activity)                   
 
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
 
-
+@bot.command()
+async def help(ctx):
+    await ctx.send('表示テストtest
+ping=動作確認用です
+mirin=まぁ…いいやつだったよ…
+まだまだ機能追加')
+                   
+@bot.command()
+async def mirin(ctx):
+    await ctx.send('まぁ…いいやつだったよ…')
+                   
 bot.run(token)
